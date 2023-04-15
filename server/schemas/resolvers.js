44d4c95904bd4990bde6717944177b40c1372740
Hailey Francis <n01402670@unf.edu>
2023-04-15 18:50:00 -0400

    added typeDefs and started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,13 @@
 //Define the query and mutation functionality to work with the Mongoose models
 
 //Hint: Use the functionality in the user-controller.js as a guide
+
+const { Book, User } = require("../models");
+
+const resolvers = {
+    Query: {
+        tech: async () => {
+            return Tech.find
+        },
+    }
+}
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,12 @@
 //Define the necessary Query and Mutation types
 
-    //Query type:
+//Query type:
         // me: returns a User type
 
     //Mutation type:
         //login: accepts an email and password as params, returns an Auth type
         //addUser: accepts a username, email, and password as params, returns an Auth type
-        //saveBook: Accepts a book author's array, description, title, bookId, image, and liknk as params, returns a User type
+        //saveBook: Accepts a book author's array, description, title, bookId, image, and link as params, returns a User type
             //(Look into creating what's known as an input type to handle all of these params!)
         //removeBook: Accepts a book's bookId as a param; returns a User type
         
@@ -28,3 +28,39 @@
     //Auth Type:
         //token
         //user (references the User type)
+
+
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+        removeButton(bookId: String!): User
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+`;
+
+module.exports = typeDefs;
+    
