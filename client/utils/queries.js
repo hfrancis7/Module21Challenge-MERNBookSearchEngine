a18296277122a993ab8a390b48e120d172d2bf70
Hailey Francis <n01402670@unf.edu>
2023-04-15 22:01:12 -0400

    started frontend updating queries file

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,1 +1,20 @@
 //this will hold the query GET_ME, which will execute the me query set up using Apollo server
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
